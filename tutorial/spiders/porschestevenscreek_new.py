e57b6b8c78bd9099124f5333ce8549c4e5f2769e
Ivan Vasilyeu <Ivan.Vasilyeu@targetmediapartners.com>
2018-10-12 15:00:54 +0200

    add vehicles

--- tutorial/spiders/porschestevenscreek_new.py
+++ tutorial/spiders/porschestevenscreek_new.py
@@ -1,7 +1,7 @@
-import scrapy
 import re
+
+import scrapy
 from scrapy import Selector
-from scrapy.http import HtmlResponse
 from scrapy.selector import SelectorList
 
 __all__ = ['Selector', 'SelectorList']
@@ -49,6 +49,8 @@ class VehicleInfo(VDPVehicleInfo):
 
 class PorscheStevensCreek(scrapy.Spider):
     name = "porschestevenscreek_new"
+    domain = 'porschestevenscreek.com'
+    allowed_domains = ['porschestevenscreek.com']
 
     def start_requests(self):
         urls = [
@@ -66,15 +68,32 @@ class PorscheStevensCreek(scrapy.Spider):
 
     def parse_model(self, listing):
         vehicle = VehicleInfo()
-        full_name = SelectorList(listing.xpath('//*[@id="vdp-title"]/div/div/div[1]/div[1]/div[1]/h1/text()')).extract_first()
+        full_name = SelectorList(
+            listing.xpath('//*[@id="vdp-title"]/div/div/div[1]/div[1]/div[1]/h1/text()')).extract_first()
         url_split = listing.url.split('/')
         ulr_path = re.sub('detail-', '', url_split[url_split.__len__() - 1])
 
         vehicle['year'] = re.findall('[0-9]{4}', ulr_path)[0]
         vehicle['make'] = re.sub('_', ' ', re.split('-', ulr_path)[1]).capitalize()
         vehicle['model'] = re.sub(vehicle['year'] + ' ' + vehicle['make'] + ' ', '', full_name)
-
-        vehicle['ext_color'] = listing.xpath('//*[@id="tab-details"]/div[3]/table/tbody/tr[1]/td[2]/text()').extract_first().strip()
+        vehicle['domain'] = self.domain
+        vehicle['trim'] = (listing.xpath('//*[@id="vdp-1-toggle"]/div[2]/div/div[2]/text()').extract_first()).strip()
+        vehicle['ext_color'] = listing.xpath(
+            '//*[@id="tab-details"]/div[3]/table/tbody/tr[1]/td[2]/text()').extract_first().strip()
+        vehicle['int_color'] = listing.xpath(
+            '//*[@id="tab-details"]/div[3]/table/tbody/tr[2]/td[2]/text()').extract_first().strip()
+        vehicle['stock_no'] = listing.xpath(
+            '//*[@id="tab-details"]/div[3]/table/tbody/tr[3]/td[2]').extract_first().strip()
+        vehicle['miles'] = "0"
+        vehicle['vin'] = listing.xpath(
+            '//*[@id="tab-details"]/div[3]/table/tbody/tr[4]/td[2]').extract_first()
+        vehicle['url'] = listing.url
+        vehicle['price'] = listing.xpath('//*[@id="vdp-price"]/div/h4/text()').extract_first().lstrip('$')
+        vehicle['veh_state'] = "new"
+        vehicle['engine'] = listing.xpath('//*[@id="tab-details"]/div[2]/table/tbody/tr[3]/td[2]').extract_first()
+        vehicle['transmission'] = listing.xpath('//*[@id="tab-details"]/div[2]/table/tbody/tr[4]/td[2]').extract_first()
+        vehicle['drivetrain'] = listing.xpath('//*[@id="tab-details"]/div[2]/table/tbody/tr[5]/td[2]').extract_first()
+        vehicle['body_style'] = listing.xpath('//*[@id="vdp-title"]/div/div/div[1]/div[1]/div[1]/h4').extract_first()
 
         image_urls = listing.css('#tab-slideshow-photos .swiper-slide a::attr(href)').extract()
 
